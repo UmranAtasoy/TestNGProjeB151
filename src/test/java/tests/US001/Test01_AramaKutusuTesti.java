4965c10f8ebcd4667041d6b210af65ebc4920c0d
umran <umran.atasoy2@gmail.com>
2023-07-24 22:35:48 +0300

    Initial commit

---
+++ src/test/java/tests/US001/Test01_AramaKutusuTesti.java
@@ -0,0 +1,10 @@
+package tests.US001;
+
+import org.testng.annotations.Test;
+
+public class Test01_AramaKutusuTesti {
+    @Test
+    public void test01() {
+
+    }
+}
---
+++ src/test/java/utilities/DataProviderUtil.java
@@ -0,0 +1,13 @@
+package utilities;
+
+import org.testng.annotations.DataProvider;
+
+public class DataProviderUtil {
+    @DataProvider
+    public Object[][] sehirVerileri() {
+        return new Object[][]{{"Ankara","IcAnadolu","06"},
+                {"Izmir","Ege","35"},
+                {"Diyarbakır","Dogu","21"}
+        };
+    }
+}
